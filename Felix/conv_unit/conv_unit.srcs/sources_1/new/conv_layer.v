b08e71e87d19e9be99261099e6df2150f1371b95
Félix Boulet <fire_yoshi_qc@hotmail.com>
2017-05-30 12:05:43 -0400

    Made the convolution layer a lot more generic.

--- Felix/conv_unit/conv_unit.srcs/sources_1/new/conv_layer.v
+++ Felix/conv_unit/conv_unit.srcs/sources_1/new/conv_layer.v
@@ -23,22 +23,35 @@
 module conv_layer
     #(
     parameter integer zero_padding = 1,
-    parameter integer stride = 2,
+    parameter integer stride = 1,
     parameter integer filter_size = 3,
     parameter integer filter_nb = 1,
-    parameter integer input_width = 9,
-    parameter integer root = 3,
-    parameter integer simd_units = 9,
-    parameter integer conv_res_size = (root+2*zero_padding) / stride
+    parameter integer input_width = 4,
+    parameter integer root = 2,
+    parameter integer conv_res_size = ((root+2*zero_padding) / stride)-filter_size+1
     )
     (
     input clk,
     input [input_width*8 - 1 : 0] image,
-    output reg [conv_res_size*conv_res_size*16-1:0] out,
-    output reg done = 0
+    input [filter_size*filter_size*filter_nb*8-1 : 0] filters,
+    output reg done = 0,
+    output reg [(round_to_next_two(filter_size*filter_size)+15)*(conv_res_size*conv_res_size) - 1 : 0] out
     );
     
-    //localparam integer conv_res_size = (root+2*zero_padding) / stride;
+    function integer round_to_next_two;
+    input integer x;
+    begin
+    x = x - 1;
+    x = x | (x >> 1);
+    x = x | (x >> 2);
+    x = x | (x >> 4);
+    x = x | (x >> 8);
+    x = x | (x >> 16);
+    x = x + 1;
+    round_to_next_two = x;
+    end
+    endfunction
+    
     integer i, j;
     integer filter_i = 0;
     integer filter_j = 0;
@@ -46,7 +59,7 @@ module conv_layer
     reg [7:0] vram [input_width + 4*root*zero_padding + 4*zero_padding*zero_padding - 1 : 0];
     reg [7:0] conv_filter [filter_size*filter_size*filter_nb - 1 : 0];
     reg [15:0] products [filter_size * filter_size - 1 : 0];
-    reg [31:0] sum = 0;
+    reg [round_to_next_two(filter_size*filter_size)+15-1:0] sum = 0;
     
     always @(posedge clk) begin
         case (operation)
@@ -55,10 +68,10 @@ module conv_layer
                     vram [i] <= 0;
                 end
                 for (j = 0; j<filter_size*filter_size*filter_nb; j = j+1) begin
-                    conv_filter [j] <= 8'h01;
+                    conv_filter [j] <= 8'hCD;
                 end
-                for (i = 0; i<16; i = i+1) begin
-                    out [i*16 +: 16] <= 0;
+                for (i = 0; i<conv_res_size*conv_res_size; i = i+1) begin
+                    out [i*(round_to_next_two(filter_size*filter_size)+15) +: round_to_next_two(filter_size*filter_size)+15] <= 0;
                 end
                 operation = 2'b01;
             end
@@ -81,19 +94,28 @@ module conv_layer
                         sum = sum + products[i*filter_size+j];
                     end
                 end
-                out [(filter_i*conv_res_size+filter_j)*16 +: 16] = sum[15:0];
+                out [(filter_i*conv_res_size+filter_j)
+                *(round_to_next_two(filter_size*filter_size)+15) 
+                +: round_to_next_two(filter_size*filter_size)+15] = sum;
                 sum = 0;
-                if (filter_j*stride < conv_res_size) begin
-                    filter_j <= filter_j + 1;
-                end
-                else if (filter_i*stride >= conv_res_size) begin
-                    filter_i <= 0;
-                    filter_j <= 0;
-                    operation = 2'b11;
+                if (filter_i < conv_res_size - 1) begin
+                    if (filter_j < conv_res_size-1) begin
+                        filter_j <= filter_j + 1;
+                    end
+                    else begin
+                        filter_j <= 0;
+                        filter_i <= filter_i + 1;
+                    end
                 end
                 else begin
-                    filter_j <= 0;
-                    filter_i <= filter_i + 1;
+                    if (filter_j < conv_res_size-1) begin
+                        filter_j <= filter_j + 1;
+                    end
+                    else begin
+                        filter_i <= 0;
+                        filter_j <= 0;
+                        operation = 2'b11;
+                    end
                 end
             end
         endcase
