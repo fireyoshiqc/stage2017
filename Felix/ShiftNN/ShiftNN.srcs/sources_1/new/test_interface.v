a7055564922eb55bdbd70ad1c359486ca6e9a050
Félix Boulet <fire_yoshi_qc@hotmail.com>
2017-07-25 11:32:31 -0400

    Modified interface to allow much better synthesizer inference, as well as customizable number of enabled units.

--- Felix/ShiftNN/ShiftNN.srcs/sources_1/new/test_interface.v
+++ Felix/ShiftNN/ShiftNN.srcs/sources_1/new/test_interface.v
@@ -22,71 +22,152 @@
 
 module test_interface
     #(
-    parameter input_size = 784,
-    parameter hidden_neurons = 2,
-    parameter output_classes = 10
+    //parameter input_size = 784,
+    parameter max_hidden_neurons = 1000,
+    parameter max_output_classes = 30,
+    parameter input_mem_size = 1024
     )
     (
-    input clk,
-    input [2:0] din,
-    input [hidden_neurons*8 -1 : 0] hdn_weights, // NEED TO CHOOSE SOMETHING TO ENTER WEIGHT DATA
-    input [output_classes*8 -1 : 0] out_weights, // NEED TO CHOOSE SOMETHING TO ENTER WEIGHT DATA
-    output [output_classes*16 - 1 : 0] out_bus // NEED TO CHOOSE SOMETHING TO OUTPUT DATA
+    input clk, ext_rst, start,
+    input [15:0] input_size,
+    input [15:0] hidden_neurons,
+    input [7:0] output_classes,
+    input [max_hidden_neurons*8 -1 : 0] hdn_weights, // NEED TO CHOOSE SOMETHING TO ENTER WEIGHT DATA
+    input [max_output_classes*8 -1 : 0] out_weights, // NEED TO CHOOSE SOMETHING TO ENTER WEIGHT DATA
+    output [max_output_classes*16 - 1 : 0] out_bus, // NEED TO CHOOSE SOMETHING TO OUTPUT DATA
+    output reg done = 1'b1
     );
     
-    integer counter = 0;
+    `include "functions.vh"
+    
     reg hdn_enb = 1'b1;
     reg out_enb = 1'b0;
     
+    reg [2:0] input_data [input_mem_size-1:0];
+    reg [clogb2(round_to_next_two(input_mem_size))-1:0] input_addr = 0;
+    reg [1:0] mode = 2'b10;
+    reg [2:0] din;
+    reg [max_hidden_neurons-1:0] henb = 0;
+    reg [max_output_classes-1:0] oenb = 0;
+    reg [15:0] h_addr = 0;
+    
+    wire [3*max_hidden_neurons - 1:0] activations;
+    reg [2:0] act_window = 0;
+    reg hrst = 1'b1;
+    reg orst = 1'b1;
+    
+    
     genvar i;
-    wire [hidden_neurons*3 - 1 : 0] activations;
-    reg [2:0] act_window;
-    reg rst = 1'b0;
-    for (i=0; i<hidden_neurons; i=i+1) begin
-        hidden_an han (.clk(clk), .rst(rst), .enb(hdn_enb), .din(din), .weight(hdn_weights[i*8 +: 8]), .activation(activations[i*3 +: 3]));
+    for (i=0; i<max_hidden_neurons; i=i+1) begin
+        hidden_an han (.clk(clk), .rst(hrst | ext_rst), .enb(henb[i]), .din(din), .weight(hdn_weights[i*8 +: 8]), .activation(activations[i*3 +: 3]));
     end
-    for (i=0; i<output_classes; i=i+1) begin
-        output_an oan (.clk(clk), .rst(rst), .enb(out_enb), .activation(act_window), .weight(out_weights[i*8 +: 8]), .acc(out_bus[i*16 +: 16]));
+    for (i=0; i<max_output_classes; i=i+1) begin
+        output_an oan (.clk(clk), .rst(orst | ext_rst), .enb(oenb[i]), .activation(act_window), .weight(out_weights[i*8 +: 8]), .acc(out_bus[i*16 +: 16]));
     end
+    
+    integer j;
+    initial begin
+        for (j=0; j<input_mem_size; j=j+1) input_data[j]=0;
+        for (j=0; j<max_hidden_neurons; j=j+1) begin
+            if (j<hidden_neurons) begin
+                henb[j] <= 1'b1;
+            end
+            else begin
+                henb[j] <= 1'b0;
+            end
+        end
+        for (j=0; j<max_output_classes; j=j+1) begin
+            if (j<output_classes) begin
+                oenb[j] <= 1'b1;
+            end
+            else begin
+                oenb[j] <= 1'b0;
+            end
+        end
+    end
+    
+    
     
     always @(posedge clk) begin
-        if (counter + 1 < input_size) begin
-            act_window <= 0;
-            hdn_enb <= 1'b1;
-            out_enb <= 1'b0;
-            rst <= 1'b0;
-            counter <= counter + 1;
-            // FETCH NEW DATA IN
-        end
-        else if (counter + 1 < (input_size + hidden_neurons)) begin
-            act_window <= activations[(counter+1-input_size)*3 +: 3];
-            hdn_enb <= 1'b0;
-            out_enb <= 1'b1;
-            rst <= 1'b0;
-            counter <= counter + 1;
+        if (ext_rst) begin
+            input_addr <= 0;
+            mode <= 1'b0;
         end
         else begin
-            act_window <= 0;
-            hdn_enb <= 1'b0;
-            out_enb <= 1'b0;
-            rst <= 1'b1;
-            counter <= 0;
+            case (mode)
+                2'b00: begin // HIDDEN LAYER
+                    hrst <= 1'b0;
+                    orst <= 1'b1;
+                    done <= 1'b0;
+                    din <= input_data[input_addr];
+                    if (input_addr + 1 < input_size) begin
+                        input_addr <= input_addr + 1;
+                        mode <= 1'b0;
+                    end
+                    else begin
+                        input_addr <= 0;
+                        mode <= 2'b01;
+                    end
+                end
+                2'b01: begin // OUTPUT LAYER
+                    din <= 0; // MAKE THE HIDDEN LAYER SAVE POWER
+                    orst <= 1'b0;
+                    hrst <= 1'b0;
+                    done <= 1'b0;
+                    act_window <= activations[h_addr*3 +: 3];
+                    if (h_addr + 1 < hidden_neurons) begin
+                        h_addr <= h_addr + 1;
+                        mode <= 1'b1;
+                    end
+                    else begin
+                        h_addr <= 0;
+                        mode <= 2'b10;
+                        done <= 1'b1;
+                    end
+                
+                end
+                2'b10: begin // DONE STATE, HOLD OUTPUT DATA
+                    din <= 0; // MAKE THE HIDDEN LAYER SAVE POWER
+                    act_window <= 3'b111; // MAKE THE OUTPUT LAYER SAVE POWER (AND DATA)
+                    if (start) begin
+                        mode <= 2'b00;
+                        hrst <= 1'b1;
+                        orst <= 1'b1;
+                    end
+                    else begin
+                        mode <= 2'b10;
+                        hrst <= 1'b0;
+                        orst <= 1'b0;
+                    end
+                
+                end
+            endcase
         end
         
     end
     
-//    always @(posedge clk) begin
-//        if (ocounter + 1 < hidden_neurons) begin
-//            //rst <= 1'b0;
-//            ocounter <= ocounter + 1;
-//            // FETCH NEW DATA IN
-//        end
-//        else begin
-//            //rst <= 1'b1;
-//            ocounter <= 1'b0;
-//        end
-        
-//        act_window <= activations[ocounter*3 +: 3];
-        
-//    end
+    always @(hidden_neurons) begin
+        for (j=0; j<max_hidden_neurons; j=j+1) begin
+            if (j<hidden_neurons) begin
+                henb[j] <= 1'b1;
+            end
+            else begin
+                henb[j] <= 1'b0;
+            end
+        end
+    end
+    
+    always @(output_classes) begin
+        for (j=0; j<max_output_classes; j=j+1) begin
+            if (j<output_classes) begin
+                oenb[j] <= 1'b1;
+            end
+            else begin
+                oenb[j] <= 1'b0;
+            end
+        end
+    end
+    
+    
+
 endmodule
