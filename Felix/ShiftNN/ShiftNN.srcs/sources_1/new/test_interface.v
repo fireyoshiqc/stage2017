8d5497849620ec93a9399340a7529437af1479af
Félix Boulet <fire_yoshi_qc@hotmail.com>
2017-08-14 11:10:34 -0400

    Added signals (addr) for BRAM and register use.

--- Felix/ShiftNN/ShiftNN.srcs/sources_1/new/test_interface.v
+++ Felix/ShiftNN/ShiftNN.srcs/sources_1/new/test_interface.v
@@ -28,16 +28,21 @@ module test_interface
     parameter input_mem_size = 1024
     )
     (
-    input clk, ext_rst, start,
-    input [15:0] input_size,
-    input [15:0] hidden_neurons,
-    input [7:0] output_classes,
-    input [max_hidden_neurons*8 -1 : 0] hdn_weights, // NEED TO CHOOSE SOMETHING TO ENTER WEIGHT DATA
-    input [max_output_classes*8 -1 : 0] out_weights, // NEED TO CHOOSE SOMETHING TO ENTER WEIGHT DATA
-    input [max_hidden_neurons*8 -1 : 0] hdn_bias,
-    input [max_output_classes*8 -1 : 0] out_bias,
-    output [max_output_classes*16 - 1 : 0] out_bus, // NEED TO CHOOSE SOMETHING TO OUTPUT DATA
-    output reg done = 1'b1
+    input clk,
+    input ext_rst, start, // From control register(s) or control block
+    input [15:0] input_size, // From register
+    input [15:0] hidden_neurons, // From register
+    input [7:0] output_classes, // From register
+    input [max_hidden_neurons*8 -1 : 0] hdn_weights, // From BRAM (load max_hidden_neurons 8-bit weights at a time, at most)
+    input [max_output_classes*8 -1 : 0] out_weights, // From BRAM (load max_output_classes 8-bit weights at a time, at most)
+    input [max_hidden_neurons*8 -1 : 0] hdn_bias, // From BRAM or register (load max_hidden_neurons 8-bit biases at a time, at most)
+    input [max_output_classes*8 -1 : 0] out_bias, // From BRAM or register (load max_output_classes 8-bit biases at a time, at most)
+    input [2:0] data_in, // From BRAM (load one 3-bit data item)
+    output reg [clogb2(round_to_next_two(input_mem_size))-1:0] data_addr = 0, // Address for data_in AND hdn_weights
+    output reg [clogb2(round_to_next_two(input_mem_size))-1:0] ow_addr = 0, // Address for out_weights
+    //output reg [clogb2(round_to_next_two(input_mem_size))-1:0] output_addr = 0, // Address for out_bus if using BRAM
+    output [max_output_classes*16 - 1 : 0] out_bus, // To BRAM or register (write max_output_classes 16-bit results at a time, at most)
+    output reg done = 1'b1 // To control register or control block
     );
     
     `include "functions.vh"
@@ -45,8 +50,8 @@ module test_interface
     reg hdn_enb = 1'b1;
     reg out_enb = 1'b0;
     
-    reg [2:0] input_data [input_mem_size-1:0];
-    reg [clogb2(round_to_next_two(input_mem_size))-1:0] input_addr = 0;
+    //reg [2:0] input_data [input_mem_size-1:0];
+    //reg [clogb2(round_to_next_two(input_mem_size))-1:0] input_addr = 0;
     reg [1:0] mode = 2'b10;
     reg [2:0] din;
     reg [max_hidden_neurons-1:0] henb = 0;
@@ -60,7 +65,8 @@ module test_interface
     
     genvar i;
     for (i=0; i<max_hidden_neurons; i=i+1) begin
-        hidden_an han (.clk(clk), .rst(hrst | ext_rst), .enb(henb[i]), .din(din), .weight(hdn_weights[i*8 +: 8]), .bias(hdn_bias[i*8 +: 8]), .activation(activations[i*3 +: 3]));
+        //hidden_an han (.clk(clk), .rst(hrst | ext_rst), .enb(henb[i]), .din(din), .weight(hdn_weights[i*8 +: 8]), .bias(hdn_bias[i*8 +: 8]), .activation(activations[i*3 +: 3]));
+        hidden_an han (.clk(clk), .rst(hrst | ext_rst), .enb(henb[i]), .din(data_in & din), .weight(hdn_weights[i*8 +: 8]), .bias(hdn_bias[i*8 +: 8]), .activation(activations[i*3 +: 3]));
     end
     for (i=0; i<max_output_classes; i=i+1) begin
         output_an oan (.clk(clk), .rst(orst | ext_rst), .enb(oenb[i]), .activation(act_window), .weight(out_weights[i*8 +: 8]), .bias(out_bias[i*8 +: 8]), .acc(out_bus[i*16 +: 16]));
@@ -68,7 +74,7 @@ module test_interface
     
     integer j;
     initial begin
-        for (j=0; j<input_mem_size; j=j+1) input_data[j]=0;
+        //for (j=0; j<input_mem_size; j=j+1) input_data[j]=0;
         for (j=0; j<max_hidden_neurons; j=j+1) begin
             if (j<hidden_neurons) begin
                 henb[j] <= 1'b1;
@@ -91,7 +97,8 @@ module test_interface
     
     always @(posedge clk) begin
         if (ext_rst) begin // EXTERNAL RESET
-            input_addr <= 0;
+            //input_addr <= 0;
+            data_addr <= 0;
             h_addr <= 0;
             din <= 0;
             act_window <= 3'b111;
@@ -120,13 +127,22 @@ module test_interface
                     hrst <= 1'b0;
                     orst <= 1'b1;
                     done <= 1'b0;
-                    din <= input_data[input_addr];
-                    if (input_addr + 1 < input_size) begin
-                        input_addr <= input_addr + 1;
+                    din <= {3{1'b1}}; // Enable hidden layer calculation
+                    //din <= input_data[input_addr];
+//                    if (input_addr + 1 < input_size) begin
+//                        input_addr <= input_addr + 1;
+//                        mode <= 2'b00;
+                    if (hrst) begin
+                        data_addr <= 0;
+                        mode <= 2'b00;
+                    end
+                    else if (data_addr + 1 < input_size) begin
+                        data_addr <= data_addr + 1;
                         mode <= 2'b00;
                     end
                     else begin
-                        input_addr <= 0;
+                        //input_addr <= 0;
+                        data_addr <= 0;
                         mode <= 2'b01;
                     end
                 end
@@ -135,13 +151,23 @@ module test_interface
                     orst <= 1'b0;
                     hrst <= 1'b0;
                     done <= 1'b0;
-                    act_window <= activations[h_addr*3 +: 3];
-                    if (h_addr + 1 < hidden_neurons) begin
-                        h_addr <= h_addr + 1;
+                    
+                    if (orst) begin
+                        ow_addr <= 0;
                         mode <= 2'b01;
+                        act_window <= activations[0 +: 3];
+                    end
+//                    if (h_addr + 1 < hidden_neurons) begin
+//                        h_addr <= h_addr + 1;
+//                        mode <= 2'b01;
+//                    end
+                    else if (ow_addr + 1 < hidden_neurons) begin
+                        ow_addr <= ow_addr + 1;
+                        mode <= 2'b01;
+                        act_window <= activations[(ow_addr + 1)*3 +: 3];
                     end
                     else begin
-                        h_addr <= 0;
+                        ow_addr <= 0;
                         mode <= 2'b10;
                         done <= 1'b1;
                     end
@@ -154,6 +180,7 @@ module test_interface
                         mode <= 2'b00;
                         hrst <= 1'b1;
                         orst <= 1'b1;
+                        //output_addr <= output_addr + 1;
                     end
                     else begin
                         mode <= 2'b10;
